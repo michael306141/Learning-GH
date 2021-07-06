21023a11adaa5a3e3fd44dbbb5ce8704caade73b
michael306141 <87039977+michael306141@users.noreply.github.com>
2021-07-06 15:06:33 -0500

    Update  ContentView to Edit in Github

--- LearningGH/ContentView.swift
+++ LearningGH/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world! CHANGE")
+        Text("Hello, world! Edit in Github")
             .padding()
     }
 }
